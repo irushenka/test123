3afce0829f8ea007bcd5b41b8535025f5cb34806
irushenka <sindirina@mail.ru>
2023-03-03 22:06:38 +0300

    animation1

--- app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
@@ -8,11 +8,14 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        findViewById<StatsView>(R.id.stats).data = listOf(
-            500F,
-            500F,
-            500F,
-            500F,
-        )
+        val view = findViewById<StatsView>(R.id.stats)
+        view.postDelayed({
+            view.data = listOf(
+                500F,
+                500F,
+                500F,
+                500F,
+            )
+        }, 3000)
     }
 }
